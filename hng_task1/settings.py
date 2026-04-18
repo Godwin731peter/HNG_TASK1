fc9a945b1d4c89d9afa871c4d4e950bc70154091
Godwin Peter <chinegbomkpapeter@gmail.com>
2026-04-18 23:22:08 +0100

    corrected typo

--- hng_task1/settings.py
+++ hng_task1/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(SECRET_KEY)
+SECRET_KEY = os.environ.get("SECRET_KEY")
 if not SECRET_KEY:
     raise ValueError('SECRET_KEY is not set')
 
