b2ff923c6564f5889ff165c8ef0056eab5afbe1c
Godwin Peter <chinegbomkpapeter@gmail.com>
2026-04-20 12:01:03 +0100

    connecting to database

--- hng_task1/settings.py
+++ hng_task1/settings.py
@@ -78,10 +78,10 @@ WSGI_APPLICATION = 'hng_task1.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
 
 REST_FRAMEWORK = {
