5f7c2073e5acf491ce7f0092660898edea50397e
Godwin Peter <chinegbomkpapeter@gmail.com>
2026-04-20 12:09:02 +0100

    import dj_database_url

--- hng_task1/settings.py
+++ hng_task1/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'task1'
+    'task1',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
